d8ac4fe1205d6dcb5723668eb644296d4c0e9549
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-02-23 01:25:18 -0500

    finished

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
-const { JWT_SECRET } = require("../secrets"); // use this secret!
+const { JWT_SECRET, BCRYPT_ROUNDS } = require("../secrets"); // use this secret!
+const jwt = require('jsonwebtoken')
+const Users = require('../users/users-model')
+const bcrypt = require('bcryptjs');
+
 
 router.post("/register", validateRoleName, (req, res, next) => {
+  const { username, password } = req.body
+  const { role_name } = req
+  const hash = bcrypt.hashSync(password, BCRYPT_ROUNDS)
+  Users.add({ username, password: hash, role_name })
+    .then(newUser => {
+      res.status(201).json(newUser)
+    })
+    .catch(next)
   /**
     [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
 
@@ -18,6 +30,20 @@ router.post("/register", validateRoleName, (req, res, next) => {
 
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
+  let { username, password } = req.body
+
+  Users.findBy({ username })
+  .then(([user]) => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+      res.status(200).json({
+        message: `${user.username} is back!`,
+        token
+      })
+    } else {
+      next({ status: 401, message: 'Invalid credentials' })
+    }
+  }).catch(next)
   /**
     [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -38,5 +64,18 @@ router.post("/login", checkUsernameExists, (req, res, next) => {
     }
    */
 });
+
+function generateToken(user) {
+  const payload = {
+      subject: user.user_id,
+      username: user.username,
+      role_name: user.role_name
+  }
+  const options = {
+      expiresIn: '1d'
+  }
+  const token = jwt.sign(payload, JWT_SECRET, options)
+  return token
+}
 
 module.exports = router;
